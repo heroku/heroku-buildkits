6cfeb0dfec70b63a09aed94e0372505e7a405f4f
Jeff Dickey <dickeyxxx@gmail.com>
2016-07-21 19:38:23 -0700

    100% code coverage

--- test/commands/publish.js
+++ test/commands/publish.js
@@ -1,5 +1,5 @@
 'use strict'
-/* globals describe it beforeEach afterEach */
+/* globals describe it beforeEach afterEach context */
 
 const cli = require('heroku-cli-util')
 const commands = require('../..').commands
@@ -8,27 +8,34 @@ const nock = require('nock')
 const expect = require('unexpected')
 
 describe('buildkits:share', () => {
-  let api, heroku
+  context('with valid args', () => {
+    let api, heroku
 
-  beforeEach(() => {
-    cli.mockConsole()
+    beforeEach(() => {
+      cli.mockConsole()
 
-    heroku = nock('https://api.heroku.com').get('/account').reply(200, {email: 'foo@foo.com'})
-    api = nock('https://buildkits.herokuapp.com')
-    api.post('/buildpacks/dickeyxxx/elixir').reply(200, {revision: '8'})
-  })
-
-  afterEach(() => {
-    api.done()
-    heroku.done()
-    nock.cleanAll()
-  })
-
-  it('publishes the elixer buildpack', () => {
-    return cmd.run({auth: {password: 'foo'}, args: {'org/name': 'dickeyxxx/elixir'}, flags: {'buildpack-dir': 'test_buildpacks/elixir'}})
-    .then(() => {
-      expect(cli.stderr, 'to equal', 'Publishing dickeyxxx/elixir buildkit... v8\n')
-      expect(cli.stdout, 'to be empty')
+      heroku = nock('https://api.heroku.com').get('/account').reply(200, {email: 'foo@foo.com'})
+      api = nock('https://buildkits.herokuapp.com')
+      api.post('/buildpacks/dickeyxxx/elixir').reply(200, {revision: '8'})
     })
+
+    afterEach(() => {
+      api.done()
+      heroku.done()
+      nock.cleanAll()
+    })
+
+    it('publishes the elixer buildpack', () => {
+      return cmd.run({auth: {password: 'foo'}, args: {'org/name': 'dickeyxxx/elixir'}, flags: {'buildpack-dir': 'test_buildpacks/elixir'}})
+      .then(() => {
+        expect(cli.stderr, 'to equal', 'Publishing dickeyxxx/elixir buildkit... v8\n')
+        expect(cli.stdout, 'to be empty')
+      })
+    })
+  })
+
+  it('errors with invalid directory', () => {
+    return expect(cmd.run({auth: {password: 'foo'}, args: {'org/name': 'dickeyxxx/elixir'}, flags: {}})
+                  , 'to be rejected with', 'Buildpack /Users/jdickey/src/github.com/heroku/heroku-buildkits missing bin/detect or bin/compile')
   })
 })
